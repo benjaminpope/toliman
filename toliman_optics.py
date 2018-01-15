8cb75aacc53d8efe29f191a2580a111c46b8e7e8
Bryn Jeffries <bryn.jeffries@sydney.edu.au>
2018-01-15 15:55:30 +1100

    Added Fresnel propagation code

--- toliman_optics.py
+++ toliman_optics.py
@@ -36,7 +36,7 @@ class EllipticalAperture(AnalyticOpticalElement):
         """ Compute the transmission inside/outside of the aperture.
         """
         if not isinstance(wave, Wavefront):  # pragma: no cover
-            raise ValueError("CircularAperture get_transmission must be called with a Wavefront "
+            raise ValueError("EllipticalAperture get_transmission must be called with a Wavefront "
                              "to define the spacing")
         assert (wave.planetype != _IMAGE)
 
@@ -52,85 +52,24 @@ class EllipticalAperture(AnalyticOpticalElement):
         self.transmission = np.ones(wave.shape)
         self.transmission[w_outside] = 0
         return self.transmission
+    
+    def get_opd(self, wave):
+        transmission = self.get_transmission(wave)
+        phase = 325e-9
+        return phase * transmission
 
-class CompositeAnalyticOptic(AnalyticOpticalElement):
-    """ Define a compound analytic optical element made up of the combination
-    of two or more individual optical elements.
-    This is just a convenience routine for semantic organization of optics.
-    It can be useful to keep the list of optical planes cleaner, but
-    you can certainly just add a whole bunch of planes all in a row without
-    using this class to group them.
-    All optics should be of the same plane type (pupil or image); propagation between
-    different optics contained inside one compound is not supported.
-    Parameters
-    ----------
-    opticslist : list
-        A list of AnalyticOpticalElements to be merged together.
-    """
+class CompositeAnalyticOptic(poppy.CompoundAnalyticOptic):
 
-    def _validate_only_analytic_optics(self, optics_list):
-        for optic in optics_list:
-            if isinstance(optic, AnalyticOpticalElement):
-                continue  # analytic elements are allowed
-            elif isinstance(optic, InverseTransmission):
-                if isinstance(optic.uninverted_optic, AnalyticOpticalElement):
-                    continue  # inverted elements are allowed, as long as they're analytic elements
-                else:
-                    return False  # inverted non-analytic elements aren't allowed, skip the rest
-            else:
-                return False  # no other types allowed, skip the rest of the list
-        return True
-
-    def __init__(self, opticslist=None, name="unnamed", verbose=True, **kwargs):
-        if opticslist is None:
-            raise ValueError("Missing required opticslist argument to CompoundAnalyticOptic")
-        AnalyticOpticalElement.__init__(self, name=name, verbose=verbose, **kwargs)
-
-        self.opticslist = []
-        self._default_display_size = 3*u.arcsec
-        self.planetype = None
-
-        for optic in opticslist:
-            if not self._validate_only_analytic_optics(opticslist):
-                raise ValueError("Supplied optics list to CompoundAnalyticOptic can "
-                                 "only contain AnalyticOptics")
-            else:
-                # if we are adding the first optic in the list, check what type of optical plane
-                # it has
-                # for subsequent optics, validate they have the same type
-                if len(self.opticslist) == 0:
-                    self.planetype = optic.planetype
-                elif (self.planetype != optic.planetype and self.planetype != PlaneType.unspecified and
-                        optic.planetype != PlaneType.unspecified):
-                    raise ValueError("Cannot mix image plane and pupil plane optics in "
-                                     "the same CompoundAnalyticOptic")
-
-                self.opticslist.append(optic)
-                if hasattr(optic, '_default_display_size'):
-                    self._default_display_size = max(self._default_display_size,
-                                                     optic._default_display_size)
-                if hasattr(optic,'pupil_diam'):
-                    if not hasattr(self,'pupil_diam'):
-                        self.pupil_diam = optic.pupil_diam
-                    else:
-                        self.pupil_diam = max(self.pupil_diam, optic.pupil_diam)
-
-        if self.planetype == _PUPIL:
-            if all([hasattr(o, 'pupil_diam') for o in self.opticslist]):
-                self.pupil_diam = np.asarray([o.pupil_diam.to(u.meter).value for o in self.opticslist]).max() * u.meter
-
+    # CompoundAnalyticOptic starts off with ones and multiplies by each element. This
+    # version starts off with zero and adds
     def get_transmission(self,wave):
         trans = np.zeros(wave.shape, dtype=np.float)
         for optic in self.opticslist:
             trans += optic.get_transmission(wave)
         return trans
 
-    def get_opd(self,wave):
-        opd = np.zeros(wave.shape, dtype=np.float)
-        for optic in self.opticslist:
-            opd += optic.get_opd(wave)
-        return opd
-
+    # CompoundAnalyticOptic starts off with ones and multiplies each optic's phasor
+    # This version starts with zeros and adds each optic's scaled phasor
     def get_phasor(self, wave):
         phasor = np.zeros(wave.shape, dtype=np.complex)
         for optic in self.opticslist:
@@ -163,3 +102,33 @@ def rosette(symm, radius, ecc, semimajor):
         aps.append(EllipticalAperture(semimajor=semimajor,ecc=ecc,shift_x=xc,shift_y=yc,rotation=180./np.pi*pa +90.,
                                       pad_factor=6))
     return CompositeAnalyticOptic(aps)
+
+def phase_rosette(edge, symm, radius, ecc, semimajor, phase):
+    """Create a custom TOLIMAN rosette
+    
+    Defines both an elliptical aperture, and a composite aperture which sums individual subapertures.
+    
+    Keyword arguments:
+    edge -- radius of edge of element
+    symm -- Degree of rotational symmetry
+    radius -- radial position of elliptical apertures (in metres)
+    ecc -- eccentricity of each aperture ellipse
+    semimajor -- length of each aperture ellipse's semimajor (in metres)
+    phase -- phase shift of rosette
+    """
+    aps = []
+    aps.append(poppy.CircularAperture(edge))
+
+    for j in np.arange(symm):
+        pa = 2.*np.pi/symm*(j+1.)
+        xc, yc = radius*np.cos(pa), radius*np.sin(pa)
+        aps.append(EllipticalAperture(semimajor=semimajor,ecc=ecc,shift_x=xc,shift_y=yc,rotation=180./np.pi*pa,
+                                      pad_factor=6))
+
+    for j in np.arange(symm):
+        pa = 2.*np.pi/symm*(j+1.) + np.pi/symm
+        xc, yc = radius*np.cos(pa), radius*np.sin(pa)
+        aps.append(EllipticalAperture(semimajor=semimajor,ecc=ecc,shift_x=xc,shift_y=yc,rotation=180./np.pi*pa +90.,
+                                      pad_factor=6))
+#    return CompositeAnalyticOptic(aps)
+    return poppy.CompoundAnalyticOptic(aps)
