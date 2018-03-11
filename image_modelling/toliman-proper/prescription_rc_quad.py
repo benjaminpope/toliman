6a88b46bd1c263e10035bc82ce40098f3878cd0e
Bryn Jeffries <bryn.jeffries@sydney.edu.au>
2018-03-12 09:41:39 +1100

    Added missing propagation step between M2 obstruction and M1

--- image_modelling/toliman-proper/prescription_rc_quad.py
+++ image_modelling/toliman-proper/prescription_rc_quad.py
@@ -1,5 +1,6 @@
 import proper
 import math
+import numpy as np
 from prop_conic import prop_conic
 from prop_tilt import prop_tilt
 
@@ -23,58 +24,77 @@ def prescription_rc_quad(wavelength, gridsize, PASSVALUE = {}):
     # Define the wavefront
     wfo = proper.prop_begin(diam, wavelength, gridsize, beam_ratio)
 
-    if proper.prop_is_statesaved(wfo) == False:
-        # Point off-axis
-        prop_tilt(wfo, tilt_x, tilt_y)
+ #   if proper.prop_is_statesaved(wfo) == False:
+    # Point off-axis
+    prop_tilt(wfo, tilt_x, tilt_y)
 
-        # Input aperture
-        proper.prop_circular_aperture(wfo, diam/2)
-        # NOTE: could prop_propagate() here if some baffling included
+    # Input aperture
+    proper.prop_circular_aperture(wfo, diam/2)
+    # NOTE: could prop_propagate() here if some baffling included
 
-        # Secondary and structs obscuration
-        proper.prop_circular_obscuration(wfo, m2_rad)                      # secondary mirror obscuration
-        # Spider struts/vanes, arranged evenly radiating out from secondary
-        strut_length = diam/2 - m2_rad
-        strut_step = 360/m2_supports
-        strut_centre = m2_rad + strut_length/2
-        for i in range(0, m2_supports):
-            angle = i*strut_step
-            radians = math.radians(angle) 
-            xoff = math.cos(radians)*strut_centre
-            yoff = math.sin(radians)*strut_centre
-            proper.prop_rectangular_obscuration(wfo, m2_strut_width, strut_length,
-                                                xoff, yoff,
-                                                ROTATION = angle + 90)
+    # Secondary and structs obscuration
+    proper.prop_circular_obscuration(wfo, m2_rad)                      # secondary mirror obscuration
+    # Spider struts/vanes, arranged evenly radiating out from secondary
+    strut_length = diam/2 - m2_rad
+    strut_step = 360/m2_supports
+    strut_centre = m2_rad + strut_length/2
+    for i in range(0, m2_supports):
+        angle = i*strut_step
+        radians = math.radians(angle) 
+        xoff = math.cos(radians)*strut_centre
+        yoff = math.sin(radians)*strut_centre
+        proper.prop_rectangular_obscuration(wfo, m2_strut_width, strut_length,
+                                            xoff, yoff,
+                                            ROTATION = angle + 90)
 
-        # Normalize wavefront
-        proper.prop_define_entrance(wfo)
+    # Normalize wavefront
+    proper.prop_define_entrance(wfo)
 
-        # Primary mirror
-        if 'm1_conic' in PASSVALUE:
-            prop_conic(wfo, m1_fl, PASSVALUE['m1_conic'], "conic primary")
-        else:
-            proper.prop_lens(wfo, m1_fl, "primary")
-        proper.prop_circular_obscuration(wfo, m1_hole_rad)
+    # Primary mirror
+    proper.prop_propagate(wfo, m1_m2_sep, "primary")
+    if 'm1_conic' in PASSVALUE:
+        prop_conic(wfo, m1_fl, PASSVALUE['m1_conic'], "conic primary")
+    else:
+        proper.prop_lens(wfo, m1_fl, "primary")
+    proper.prop_circular_obscuration(wfo, m1_hole_rad)
 
+    if 'phase_func' in PASSVALUE:
+        phase_func = PASSVALUE['phase_func']
+        ngrid = proper.prop_get_gridsize(wfo)
+        sampling = proper.prop_get_sampling(wfo)
+#            beamradius = proper.prop_get_beamradius(wfo)
+        phase_map = np.zeros([ngrid, ngrid], dtype = np.float64)
+        c = ngrid/2.
+        for i in range(ngrid):
+            for j in range(ngrid):
+                x = i - c
+                y = j - c
+                phi = math.atan2(y, x)
+                r = sampling*math.hypot(x,y)
+                phase_map[i][j] = phase_func(r, phi)
+        proper.prop_add_phase(wfo, phase_map)
 
-        # Propagate the wavefront
-        proper.prop_propagate(wfo, m1_m2_sep, "secondary")
+    # Secondary mirror
+    proper.prop_propagate(wfo, m1_m2_sep, "secondary")
+    if 'm1_conic' in PASSVALUE:
+        prop_conic(wfo, m2_fl, PASSVALUE['m2_conic'], "conic secondary")
+    else:
+        proper.prop_lens(wfo, m2_fl, "secondary")
+    proper.prop_circular_aperture(wfo, m2_rad)    
 
-        # Secondary mirror
-        if 'm1_conic' in PASSVALUE:
-            prop_conic(wfo, m2_fl, PASSVALUE['m2_conic'], "conic secondary")
-        else:
-            proper.prop_lens(wfo, m2_fl, "secondary")
-        proper.prop_circular_aperture(wfo, m2_rad)    
+#    proper.prop_state(wfo)
 
-        # Hole through primary
+    # Hole through primary
+    if m1_m2_sep<bfl:
         proper.prop_propagate(wfo, m1_m2_sep, "M1 hole")
         proper.prop_circular_aperture(wfo, m1_hole_rad)    
 
-    proper.prop_state(wfo)
 
     # Focus - bfl can be varied between runs
-    proper.prop_propagate(wfo, bfl-m1_m2_sep, "focus", TO_PLANE=True)
+    if m1_m2_sep<bfl:
+        proper.prop_propagate(wfo, bfl-m1_m2_sep, "focus", TO_PLANE=True)
+    else:
+        proper.prop_propagate(wfo, bfl, "focus", TO_PLANE=True)
 
     # End
     return proper.prop_end(wfo, NOABS = noabs)
