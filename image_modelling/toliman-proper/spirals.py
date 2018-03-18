3ca17387edf3d361ffb327f9a9347f863263876a
Bryn Jeffries <bryn.jeffries@sydney.edu.au>
2018-03-19 09:54:58 +1100

    Relabelled some parameters and eliminated some repeated computations

--- image_modelling/toliman-proper/spirals.py
+++ image_modelling/toliman-proper/spirals.py
@@ -1,7 +1,7 @@
 import numpy as np
 from math import sin, log10, cos, atan2, hypot
 
-def binarized_ringed(r, phi, thresh=0., wl=6e-6):
+def binarized_ringed(r, phi, phase, thresh=0., white=0, empty=0.):
     # Spiral parameters
     alpha1 = 20.186
     m1 = 5
@@ -13,20 +13,22 @@ def binarized_ringed(r, phi, thresh=0., wl=6e-6):
     alpha3 = 4.0372
     eta3 = -0.575    
 
-    s = 0.15/300. # m/internal sampling dist
+    # Calculations assume a space with fixed radii 
+    # parameters. These can be scaled to physical units
+    scale = 0.15/300. # m/internal sampling dist
     # Physical dimensions
     r_max = 300.
     r_min = 50.
     r_split = 246. # interface between main sprial and outer rim    
     
-    white = 0
-    black = wl/4. # metres
-    v = white
-    r = r/s
+    black = phase
+    v = empty
+    r = r/scale
     if (r<=r_max and r>r_min):
-        c1 = cos(alpha1*log10(r)+m1*phi+eta1)
-        c2 = cos(alpha2*log10(r)+m2*phi+eta2)
-        c3 = sin(alpha3*log10(r)+m3*phi+eta3)
+        logr = log10(r)
+        c1 = cos(alpha1*logr+m1*phi+eta1)
+        c2 = cos(alpha2*logr+m2*phi+eta2)
+        c3 = sin(alpha3*logr+m3*phi+eta3)
         if (r>r_split): # Outer rim
             v=white if (c3<thresh or c1*c2*c3>thresh) else black
         else: # Main spiral
@@ -51,22 +53,20 @@ def binarized_ringed_flipped(r, phi, phase, thresh=0., white=0, empty=0.):
     r_min = 50.
     r_split = 246. # interface between main sprial and outer rim    
     
-    #white = 0
-    black = phase# wl/4. # metres
-    #grey = (black - white)/2.
+    black = phase
     v = empty
     r = r/s
     if (r<=r_max and r>r_min):
-        a1 = alpha1*log10(r)+m1*phi+eta1
-        c1 = cos(a1)
-        a2 = alpha2*log10(r)+m2*phi+eta2
-        c2 = cos(a2)
-        a3 = alpha3*log10(r)+m3*phi+eta3
-        c3 = sin(a3)
-        s3 = sin(a3/2.)
+        logr = log10(r)
+        chi1 = alpha1*logr+m1*phi+eta1
+        c1 = cos(chi1)
+        chi2 = alpha2*logr+m2*phi+eta2
+        c2 = cos(chi2)
+        chi3 = alpha3*logr+m3*phi+eta3
+        c3 = sin(chi3)
         if (r>r_split): # Outer rim
             if (c3<thresh):
-                if (s3<thresh):
+                if (sin(chi3/2.)<thresh):
                     v=black if (c1*c2*c3>thresh) else white
                 else:
                     v=black                
