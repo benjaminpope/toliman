a4a70575382384e7cda7afc1d2ebd49ba07e833e
Bryn Jeffries <bryn.jeffries@sydney.edu.au>
2018-04-11 09:44:14 +1000

    Adding new notebook to verify spiral pupil PSF against Zemax image

--- image_modelling/zemax_examples/import_zemax.py
+++ image_modelling/zemax_examples/import_zemax.py
@@ -3,7 +3,7 @@ import codecs
 import os
 import sys
 
-for filename in sys.argv[1:]
+for filename in sys.argv[1:]:
     with codecs.open(filename, encoding='utf-16') as f:
         data = np.loadtxt(f, skiprows=21)
     # Export it as a more convenient (and compact) numpy binary file
